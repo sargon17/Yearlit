3284ad972e47001a0bb9cb7961bdbacb907d8324
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-03-29 22:03:55 +0100

    Refactor CalendarStatisticsView and update localization strings
    - Enhance CalendarStatisticsView by introducing a new layout with StatisticItem components for better visual representation of statistics.
    - Remove unused localization entries related to statistics and active days to streamline the localization file.
    - Update the CustomCalendarView to utilize the new CalendarStatisticsView structure, improving code organization and readability.

--- My Year/Views/CustomCalendars/CalendarStatisticsView.swift
+++ My Year/Views/CustomCalendars/CalendarStatisticsView.swift
@@ -10,11 +10,165 @@ struct CalendarStats {
 
 struct CalendarStatisticsView: View {
   let stats: CalendarStats
+  let accentColor: Color
 
   var body: some View {
-    VStack {
-      Text("Calendar Statistics")
-    }
+    VStack(spacing: 0) {
+      VStack(spacing: 16) {
+        CustomSeparator()
+        HStack {
+          Text("Statistics")
+            .font(.system(size: 36, design: .monospaced))
+            .foregroundColor(Color("text-primary"))
+            .fontWeight(.black)
+            .padding(.horizontal)
+
+          Spacer()
+        }
+        CustomSeparator()
+      }
+      StatisticItem(title: "Active Days", value: "\(stats.activeDays)", accentColor: accentColor)
+      StatisticItem(title: "Total Times Logged", value: "\(stats.totalCount)", accentColor: accentColor)
+      StatisticItem(title: "Max Times Logged in a Day", value: "\(stats.maxCount)", accentColor: accentColor)
+      StatisticItem(title: "Longest Days in a Row", value: "\(stats.longestStreak)", accentColor: accentColor)
+      StatisticItem(title: "Current Days in a Row", value: "\(stats.currentStreak)", accentColor: accentColor)
+    }.padding(.bottom)
   }
 }
 
+struct StatisticItem: View {
+  let title: String
+  let value: String
+  let accentColor: Color
+
+  var body: some View {
+    VStack(spacing: 0) {
+        HStack(alignment: .center) {
+          Text(title)
+            .font(.system(size: 12, design: .monospaced))
+            .foregroundColor(Color("text-tertiary"))
+
+          Spacer()
+          Text(value)
+            .font(.system(size: 64, design: .monospaced))
+            .foregroundColor(Color(accentColor))
+            .fontWeight(.black)
+            .padding(.bottom, -20)
+        }
+        .clipped()
+        .padding(0)
+        .padding(.horizontal)
+        .overlay(
+          VStack {
+            Spacer()
+            CustomSeparator()
+          }
+        ).frame(maxWidth: .infinity)
+      }
+  }
+}
+
+// HStack {
+//           VStack {
+//             VStack(alignment: .center, spacing: 4) {
+//               Text("Days")
+//                 .font(.system(size: 10))
+//                 .foregroundColor(Color("text-tertiary"))
+
+//               VStack(alignment: .center) {
+//                 Text("\(stats.activeDays)")
+//                   .font(.system(size: 18))
+//                   .foregroundColor(Color("text-secondary"))
+//                   .fontWeight(.black)
+
+//                 Text("Active")
+//                   .font(.system(size: 10))
+//                   .foregroundColor(Color("text-tertiary").opacity(0.5))
+
+//               }
+//             }.padding(10)
+//           }
+//           .frame(maxWidth: .infinity)
+//           .background(Color("surface-secondary").opacity(0.5))
+//           .cornerRadius(10)
+
+//           Spacer()
+
+//           if calendar.trackingType != .binary {
+
+//             VStack {
+//               VStack(alignment: .center, spacing: 4) {
+//                 Text("Count")
+//                   .font(.system(size: 10))
+//                   .foregroundColor(Color("text-tertiary"))
+
+//                 HStack {
+//                   VStack(alignment: .center) {
+//                     Text("\(stats.totalCount)")
+//                       .font(.system(size: 18))
+//                       .foregroundColor(Color("text-secondary"))
+//                       .fontWeight(.black)
+
+//                     Text("Total")
+//                       .font(.system(size: 10))
+//                       .foregroundColor(Color("text-tertiary").opacity(0.5))
+//                   }.frame(maxWidth: .infinity)
+
+//                   VStack(alignment: .center) {
+//                     Text("\(stats.maxCount)")
+//                       .font(.system(size: 18))
+//                       .foregroundColor(Color("text-secondary"))
+//                       .fontWeight(.black)
+
+//                     Text("Max")
+//                       .font(.system(size: 10))
+//                       .foregroundColor(Color("text-tertiary").opacity(0.5))
+//                   }.frame(maxWidth: .infinity)
+
+//                 }
+//               }
+//               .padding(10)
+//             }
+//             .frame(maxWidth: .infinity)
+//             .background(Color("surface-secondary").opacity(0.5))
+//             .cornerRadius(10)
+//           }
+
+//           VStack {
+//             VStack(alignment: .center, spacing: 4) {
+//               Text("Streaks")
+//                 .font(.system(size: 10))
+//                 .foregroundColor(Color("text-tertiary"))
+
+//               HStack {
+//                 VStack(alignment: .center) {
+//                   Text("\(stats.currentStreak)")
+//                     .font(.system(size: 18))
+//                     .foregroundColor(Color("text-primary"))
+//                     .fontWeight(.black)
+
+//                   Text("Current")
+//                     .font(.system(size: 10))
+//                     .foregroundColor(Color("text-tertiary").opacity(0.5))
+//                 }.frame(maxWidth: .infinity)
+
+//                 VStack(alignment: .center) {
+//                   Text("\(stats.longestStreak)")
+//                     .font(.system(size: 18))
+//                     .foregroundColor(Color("text-secondary"))
+//                     .fontWeight(.black)
+
+//                   Text("Longest")
+//                     .font(.system(size: 10))
+//                     .foregroundColor(Color("text-tertiary").opacity(0.5))
+//                 }.frame(maxWidth: .infinity)
+
+//               }
+//             }
+//             .padding(10)
+//           }
+//           .frame(maxWidth: .infinity)
+//           .background(Color("surface-secondary").opacity(0.5))
+//           .cornerRadius(10)
+//         }
+//         .padding(.horizontal)
