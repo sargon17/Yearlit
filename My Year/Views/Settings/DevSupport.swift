47ece36b4555656d2e7a423799cc6a99315319d8
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-09-17 10:53:41 +0200

    things

--- My Year/Views/Settings/DevSupport.swift
+++ My Year/Views/Settings/DevSupport.swift
@@ -1,16 +1,34 @@
+import RevenueCat
+import RevenueCatUI
 import StoreKit
 import SwiftUI
+import SwiftfulRouting
 import UIKit
 
 struct DevSupport: View {
+  @Environment(\.router) var router
+
+  @State private var customerInfo: CustomerInfo?
+
+  func isPremium() -> Bool {
+    return customerInfo?.entitlements["premium"]?.isActive ?? false
+  }
+
   var body: some View {
-    Section(header: Text("Support")) {
+    Section(header: Text("Support the Developer")) {
       Button("Leave a Review") {
         if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
           if #available(iOS 18.0, *) {
             AppStore.requestReview(in: scene)
           } else {
             SKStoreReviewController.requestReview(in: scene)
+          }
+        }
+      }
+      if !isPremium() {
+        Button("Get PRO") {
+          router.showScreen(.sheet) { AnyRouter in
+            PaywallView()
           }
         }
       }
