3df5a612a3c4f45f101280fe22eb3441b7eec4bc
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2025-12-28 17:48:26 +0100

    refactor: Removed the startup app-group diagnostic write and synchronize() from My Year/My_YearApp.swift, so launch no longer forces a disk flush.

--- My Year/My_YearApp.swift
+++ My Year/My_YearApp.swift
@@ -28,24 +28,6 @@ struct My_YearApp: App {
   init() {
     Purchases.configure(withAPIKey: "appl_rQKHOkYUqJKaipHpcSXlIpPgvPe")
 
-    let appGroupId = "group.sargon17.My-Year"
-    if let defaults = UserDefaults(suiteName: appGroupId) {
-      NSLog("Successfully created UserDefaults with App Group: \(appGroupId)")
-
-      let testKey = "appLaunchTest"
-      defaults.set("test", forKey: testKey)
-      defaults.synchronize()
-
-      if let testValue = defaults.string(forKey: testKey) {
-        NSLog("Successfully wrote and read test value: \(testValue)")
-        defaults.removeObject(forKey: testKey)
-      } else {
-        NSLog("Failed to read test value!")
-      }
-    } else {
-      NSLog("Failed to create UserDefaults with App Group!")
-    }
-
     // * Reviews Promt Manager
     print("start review prompter")
     ReviewPrompter.shared.rules = .init(
