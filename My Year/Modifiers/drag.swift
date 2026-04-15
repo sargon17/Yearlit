afcee7e73ad1203435a44f448378d7f4f0b2e925
Mykhaylo <63978197+sargon17@users.noreply.github.com>
2026-04-15 17:15:05 +0200

    chore: fix build issue

--- My Year/Modifiers/drag.swift
+++ My Year/Modifiers/drag.swift
@@ -27,8 +27,8 @@ struct ContextOrDragModifier: ViewModifier {
                 Text("Edit Calendar")
             }
             Divider()
-            Button(action: toggleArchiveStatus) {
-                Text(calendar.isArchived ? "Unarchive Calendar" : "Archive Calendar")
+            Button(action: archiveCalendar) {
+                Text("Archive Calendar")
             }
             Divider()
             Button(action: {
@@ -39,10 +39,12 @@ struct ContextOrDragModifier: ViewModifier {
         }
     }
 
-    private func toggleArchiveStatus() {
+    private func archiveCalendar() {
+        guard !calendar.isArchived else { return }
+
         var updatedCalendar = calendar
-        updatedCalendar.isArchived.toggle()
-        scheduleNotifications(for: updatedCalendar, store: store)
+        updatedCalendar.isArchived = true
+        rescheduleNotifications(for: updatedCalendar, store: store)
         withAnimation(.spring(response: 0.35, dampingFraction: 0.85)) {
             store.updateCalendar(updatedCalendar)
         }
